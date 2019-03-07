a2037a9b88fc88278f08557c43251a9e8d1ca7c3
Vi Pro <VdustR@gmail.com>
2019-03-08 01:36:14 +0800

    many things
    - remove env
    - navlink with icon
    - add followed twitter accounts

---
+++ src/.vuepress/theme/components/NavLink.vue
@@ -0,0 +1,69 @@
+<template lang="pug">
+router-link.nav-link(:to='link', v-if='!isExternal(link)', :exact='exact') {{ item.text }}
+a.nav-link.external(v-else='', :href='link', :target="isMailto(link) || isTel(link) ? null : '_blank'", :rel="isMailto(link) || isTel(link) ? null : 'noopener noreferrer'")
+  template(v-if="icons[item.text]")
+    img.my-icon(:alt="item.text" :src="icons[item.text]")
+  | {{ item.text }}
+  OutboundLink
+</template>
+
+<script>
+import {
+  isExternal,
+  isMailto,
+  isTel,
+  ensureExt,
+} from '@vuepress/theme-default/util';
+import trophyIcon from '@fortawesome/fontawesome-free/svgs/solid/trophy.svg';
+import discordIcon from '@fortawesome/fontawesome-free/svgs/brands/discord.svg';
+import githubIcon from '@fortawesome/fontawesome-free/svgs/brands/github.svg';
+
+export default {
+  data() {
+    return {
+      icons: {
+        風雲榜: trophyIcon,
+        Discord: discordIcon,
+        GitHub: githubIcon,
+      },
+    };
+  },
+  props: {
+    item: {
+      required: true,
+    },
+  },
+
+  computed: {
+    link() {
+      return ensureExt(this.item.link);
+    },
+
+    exact() {
+      if (this.$site.locales) {
+        return Object.keys(this.$site.locales).some(
+          rootLink => rootLink === this.link
+        );
+      }
+      return this.link === '/';
+    },
+  },
+
+  methods: {
+    isExternal,
+    isMailto,
+    isTel,
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.my-icon
+  width: 1em
+  transform: scale(1.5)
+  margin: 0 0.5em
+
+.nav-link
+  display: inline-flex
+  align-items: center
+</style>
